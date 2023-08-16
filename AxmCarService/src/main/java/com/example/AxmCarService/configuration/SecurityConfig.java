7d2ed37cc73ab1f734b5dfe7556a533c4236fc3e
damdravic <damian.dragosv@gmail.com>
2023-08-16 18:20:47 +0300

    ...

--- AxmCarService/src/main/java/com/example/AxmCarService/configuration/SecurityConfig.java
+++ AxmCarService/src/main/java/com/example/AxmCarService/configuration/SecurityConfig.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +20,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @RequiredArgsConstructor
+@EnableMethodSecurity
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     private BCryptPasswordEncoder passwordEncoder;
